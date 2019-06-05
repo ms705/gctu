a8226c1bbaa4267d4b913062850dc584478c9454
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-06-04 23:58:33 -0400

    Add enum for "missing info" field of job events

--- src/job_events.rs
+++ src/job_events.rs
@@ -9,13 +9,44 @@
 #[derive(Debug, Deserialize)]
 pub struct JobEvent {
     pub time: u64,
-    pub missing_info: Option<u8>,
+    pub missing_info: Option<MissingInfo>,
     pub job_id: u64,
     pub event_type: u8,
     pub user: Option<String>,
     pub scheduling_class: Option<u8>,
     pub job_name: Option<String>,
     pub logical_job_name: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum MissingInfo {
+    /// (0): "we did not find a record representing the given event, but a later snapshot of the job
+    /// or task state indicated that the transition must have occurred. The timestamp of the
+    /// synthesized event is the timestamp of the snapshot."
+    SnapshotButNoTransition,
+    /// (1): "we did not find a record representing the given termination event, but the job or task
+    /// disappeared from later snapshots of cluster states, so it must have been terminated. The
+    /// timestamp of the synthesized event is a pessimistic upper bound on its actual termination
+    /// time assuming it could have legitimately been missing from one snapshot."
+    NoSnapshotOrTransition,
+    /// (2): "we did not find a record representing the creation of the given task or job. In this
+    /// case, we may be missing metadata (job name, resource requests, etc.) about the job or task
+    /// and we may have placed SCHEDULE or SUBMIT events latter than they actually are."
+    ExistsButNoCreation,
+}
+
+impl Into<MissingInfo> for &str {
+    fn into(self) -> MissingInfo {
+        if self == "0" {
+            MissingInfo::SnapshotButNoTransition
+        } else if self == "1" {
+            MissingInfo::NoSnapshotOrTransition
+        } else if self == "2" {
+            MissingInfo::ExistsButNoCreation
+        } else {
+            unreachable!()
+        }
+    }
 }
 
 pub fn for_each_in_file<F>(file: &str, mut f: F) -> std::io::Result<()>
