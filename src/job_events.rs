b24aba61b2873ac3889cae41fc291aab0a684bdc
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-06-09 23:13:24 -0700

    Turn job event type into enum

--- src/job_events.rs
+++ src/job_events.rs
@@ -11,11 +11,43 @@ pub struct JobEvent {
     pub time: u64,
     pub missing_info: Option<MissingInfo>,
     pub job_id: u64,
-    pub event_type: u8,
+    pub event_type: JobEventType,
     pub user: Option<String>,
     pub scheduling_class: Option<u8>,
     pub job_name: Option<String>,
     pub logical_job_name: Option<String>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum JobEventType {
+    /// SUBMIT (0): A task or job became eligible for scheduling.
+    Submit,
+    /// SCHEDULE (1): A job or task was scheduled on a machine. (It may not start running
+    /// immediately due to code-shipping time, etc.) For jobs, this occurs the first time any task
+    /// of the job is scheduled on a machine.
+    Schedule,
+    /// EVICT(2): A task or job was descheduled because of a higher priority task or job, because
+    /// the scheduler overcommitted and the actual demand exceeded the machine capacity, because
+    /// the machine on which it was running became unusable (e.g. taken offline for repairs), or
+    /// because a disk holding the task’s data was lost.
+    Evict,
+    /// FAIL(3): A task or job was descheduled (or, in rare cases, ceased to be eligible for
+    /// scheduling while it was pending) due to a task failure.
+    Fail,
+    /// FINISH(4): A task or job completed normally.
+    Finish,
+    /// KILL(5): A task or job was cancelled by the user or a driver program or because another job
+    /// or task on which this job was dependent died.
+    Kill,
+    /// LOST(6): A task or job was presumably terminated, but a record indicating its termination
+    /// was missing from our source data.
+    Lost,
+    /// UPDATE_PENDING(7): A task or job’s scheduling class, resource requirements, or constraints
+    /// were updated while it was waiting to be scheduled.
+    UpdatePending,
+    /// UPDATE_RUNNING(8): A task or job’s scheduling class, resource requirements, or constraints
+    /// were updated while it was scheduled.
+    UpdateRunning,
 }
 
 #[derive(Debug, Deserialize)]
