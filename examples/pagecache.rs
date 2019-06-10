fc5fd260c547792ff4f4cbba8e90ed4ef7b210f2
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-06-09 23:13:10 -0700

    rustfmt

--- examples/pagecache.rs
+++ examples/pagecache.rs
@@ -6,10 +6,15 @@ use hdrhistogram::Histogram;
 fn print(label: &str, hist: &hdrhistogram::Histogram<u64>) {
     println!("\n# {} samples: {}", label, hist.len());
     println!("median: {}", hist.value_at_quantile(0.5) as f64 / 10000.0);
-    println!("90th percentile: {}", hist.value_at_quantile(0.9) as f64 / 10000.0);
-    println!("99th percentile: {}", hist.value_at_quantile(0.99) as f64 / 10000.0);
+    println!(
+        "90th percentile: {}",
+        hist.value_at_quantile(0.9) as f64 / 10000.0
+    );
+    println!(
+        "99th percentile: {}",
+        hist.value_at_quantile(0.99) as f64 / 10000.0
+    );
 }
-
 
 fn main() -> std::io::Result<()> {
     let mut canon = Histogram::<u64>::new_with_bounds(1, 10000, 2).unwrap();
@@ -21,20 +26,29 @@ fn main() -> std::io::Result<()> {
 
     task_usage::for_each_in_file(f, |task_usage: TaskUsageRecord| -> std::io::Result<()> {
         if let Some(cmu) = task_usage.canonical_mem_usage {
-            canon.record((cmu * 10000.0) as u64).expect("recording to histogram failed");
+            canon
+                .record((cmu * 10000.0) as u64)
+                .expect("recording to histogram failed");
         }
         if let Some(amu) = task_usage.assigned_mem_usage {
-            assigned.record((amu * 10000.0) as u64).expect("recording to histogram failed");
+            assigned
+                .record((amu * 10000.0) as u64)
+                .expect("recording to histogram failed");
         }
         if let Some(upc) = task_usage.unmapped_page_cache {
-            unmapped_pc.record((upc * 10000.0) as u64).expect("recording to histogram failed");
+            unmapped_pc
+                .record((upc * 10000.0) as u64)
+                .expect("recording to histogram failed");
         }
         if let Some(tpc) = task_usage.total_page_cache {
-            total_pc.record((tpc * 10000.0) as u64).expect("recording to histogram failed");
+            total_pc
+                .record((tpc * 10000.0) as u64)
+                .expect("recording to histogram failed");
         }
-    
+
         Ok(())
-    }).expect("failed to process task usage data");
+    })
+    .expect("failed to process task usage data");
 
     print("canon. mem usage", &canon);
     print("assigned mem usage", &assigned);
