b87c51dfa3104c87bf032ea5eefa94707a1a1e08
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-06-13 13:04:38 -0400

    Read machine configs and de-normalize results
    All memory numbers in the trace are normalized to the memory of the
    largest machine in the cluster. Dividing the usage numbers by the
    memory capacity of the machine (itself normalized to the largest
    machine), we end up with fractions of actual machine memory.

--- examples/pagecache.rs
+++ examples/pagecache.rs
@@ -1,7 +1,9 @@
 extern crate gctu;
 
+use gctu::machine_events::{self, MachineEvent};
 use gctu::task_usage::{self, TaskUsageRecord};
 use hdrhistogram::Histogram;
+use std::collections::HashMap;
 
 static TRACE_START_TIME: u64 = 0u64;
 
@@ -57,6 +59,25 @@ fn main() -> std::io::Result<()> {
     let mut unmapped_pc = Histogram::<u64>::new_with_bounds(1, 10000, 2).unwrap();
     let mut total_pc = Histogram::<u64>::new_with_bounds(1, 10000, 2).unwrap();
 
+    let mut active_machines = HashMap::new();
+
+    let mf = format!("{}/machine_events/part-00000-of-00500.csv", trace_path);
+    machine_events::for_each_in_file(&mf, |machine_event: MachineEvent| -> std::io::Result<()> {
+        if initial_only && machine_event.time > TRACE_START_TIME {
+            // XXX(malte): return and indication to stop iterating
+            return Ok(());
+        }
+
+        if machine_event.event_type == 1u8 {
+            if let Some(mf) = machine_event.memory {
+                active_machines.insert(machine_event.machine_id, mf);
+            }
+        }
+
+        Ok(())
+    })
+    .expect("failed to process machine events");
+
     let f = format!("{}/task_usage/part-00000-of-00500.csv", trace_path);
 
     task_usage::for_each_in_file(&f, |task_usage: TaskUsageRecord| -> std::io::Result<()> {
@@ -65,22 +86,30 @@ fn main() -> std::io::Result<()> {
             return Ok(());
         }
 
+        let mem_frac = active_machines
+            .get(&task_usage.machine_id)
+            .expect(&format!("Machine {} does not exist", task_usage.machine_id));
+
         if let Some(cmu) = task_usage.canonical_mem_usage {
+            let cmu = cmu / mem_frac;
             canon
                 .record((cmu * 10000.0) as u64)
                 .expect("recording to histogram failed");
         }
         if let Some(amu) = task_usage.assigned_mem_usage {
+            let amu = amu / mem_frac;
             assigned
                 .record((amu * 10000.0) as u64)
                 .expect("recording to histogram failed");
         }
         if let Some(upc) = task_usage.unmapped_page_cache {
+            let upc = upc / mem_frac;
             unmapped_pc
                 .record((upc * 10000.0) as u64)
                 .expect("recording to histogram failed");
         }
         if let Some(tpc) = task_usage.total_page_cache {
+            let tpc = tpc / mem_frac;
             total_pc
                 .record((tpc * 10000.0) as u64)
                 .expect("recording to histogram failed");
